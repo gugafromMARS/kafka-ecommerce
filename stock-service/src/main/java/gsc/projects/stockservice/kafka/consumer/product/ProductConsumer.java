1f3aaad82e264a6494624bf16f555588a5bdf381
Gonçalo <goncaloscravo@gmail.com>
2023-12-01 14:30:15 +0000

    Add: stock service implementation

--- stock-service/src/main/java/gsc/projects/stockservice/kafka/consumer/product/ProductConsumer.java
+++ stock-service/src/main/java/gsc/projects/stockservice/kafka/consumer/product/ProductConsumer.java
@@ -1,20 +1,25 @@
-package gsc.projects.stockservice.kafka.consumer;
+package gsc.projects.stockservice.kafka.consumer.product;
 
-import gsc.projects.basedomains.events.OrderEvent;
 import gsc.projects.basedomains.events.ProductEvent;
+import gsc.projects.stockservice.kafka.consumer.order.OrderConsumer;
+import gsc.projects.stockservice.service.StockServiceImp;
+import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class ProductConsumer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+    private final StockServiceImp stockServiceImp;
 
 
     @KafkaListener(groupId = "${spring.kafka.consumer.group-id}", topics = "topic_productzzz")
     public void consume(ProductEvent productEvent){
-        LOGGER.info(String.format("Order event received at stock service -> %s", productEvent.toString()));
+        LOGGER.info(String.format("Product event received at stock service -> %s", productEvent.toString()));
+        stockServiceImp.saveProduct(productEvent);
     }
 }
