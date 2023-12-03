8c6955ccba0803d4c2d649c12fe484f632381ac9
Gonçalo <goncaloscravo@gmail.com>
2023-12-03 21:41:21 +0000

    Add: user service module

---
+++ user-service/src/test/java/gsc/projects/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package gsc.projects.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
