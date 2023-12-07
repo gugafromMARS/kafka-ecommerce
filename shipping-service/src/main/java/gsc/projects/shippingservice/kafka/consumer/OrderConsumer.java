018707fac9f8b9f47502768679552475b03f8f2c
Gonçalo <goncaloscravo@gmail.com>
2023-12-07 12:55:02 +0000

    Add: kafka implementation v2

--- shipping-service/src/main/java/gsc/projects/shippingservice/kafka/consumer/OrderConsumer.java
+++ shipping-service/src/main/java/gsc/projects/shippingservice/kafka/consumer/OrderConsumer.java
@@ -2,18 +2,24 @@ package gsc.projects.shippingservice.kafka.consumer;
 
 
 import gsc.projects.basedomains.events.ShippingOrder;
+import gsc.projects.shippingservice.kafka.producer.ShippingProducer;
+import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class OrderConsumer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+    private final ShippingProducer shippingProducer;
 
     @KafkaListener(groupId = "${spring.kafka.consumer.group-id}", topics = "topic_shipping")
     public void consume(ShippingOrder shippingOrder){
         LOGGER.info(String.format("Shipping order received from order service -> %s", shippingOrder.toString()));
+
+        shippingProducer.send(shippingOrder);
     }
 }
